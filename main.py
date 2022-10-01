8244dabdcfb8ced174635755ef98496abc388c4c
Aditya Desai <94288784+adidesai9009@users.noreply.github.com>
2022-10-01 23:45:11 +0530

    added otp function

--- main.py
+++ main.py
@@ -10,6 +10,13 @@ url = "https://api.ultramsg.com/instance14131/messages/chat"
 async def root():
     return "ok"
 
+@app.post("/otp")
+async def get_body():
+    payload = f"token=j6zub8czi2qjp57p&to=+917720063009,&body=otp&priority=10&referenceId="
+    headers = {'content-type': 'application/x-www-form-urlencoded'}
+    requests.request("POST", url, data=payload, headers=headers)
+    return ""
+
 @app.post("/blog")
 async def get_body(request: Request):
     data=await request.json()
